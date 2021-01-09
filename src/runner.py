7d8c9123f31d50ecc96375c5345e788818c71af7
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2021-01-09 23:40:20 +0900

    first commit from WindowsPC

--- src/runner.py
+++ src/runner.py
@@ -1,11 +1,9 @@
 from src import app
-import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from pytube import extract
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
